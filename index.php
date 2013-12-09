dceb35a20305c27783c259516372b60e3b8f0d66
jonathandrivet <jonathand.rivet@hotmail.com>
2013-12-09 01:27:47 -0500

    Petites modifs
    Pour le style encore une fois, mais aussi pour le français et la présentation

--- index.php
+++ index.php
@@ -14,54 +14,53 @@
 
 <br />
 <form method="post" action="analyse.php" >
-  <h1>WikiContrib</h1><a href="http://fr.wikipedia.org"><img class="wikilogo" src="img/wiki.png" /></a>
-  <div class="inset">
-  <p>
-    <label for="nom">NOM DU COMPTE :</label>
-    <input type="text" name="nom" id="nom">
-  </p>
-  <p>
-    <label for="wikisite">QUEL SITE UTILISER :</label>
-    <SELECT name="wikisite">
-    <OPTION selected value="http://fr.wikipedia.org">fr.wikipedia.org</OPTION>
-    <OPTION value="http://en.wikipedia.org">en.wikipedia.org</OPTION>
-    </SELECT>
-  </p>
-  <p>
-    <label for="annee">POUR QUEL ANNÉE ?</label>
-    <SELECT name="annee">
-	<OPTION value="2014">2014</OPTION>
-    <OPTION selected value="2013">2013</OPTION>
-    <OPTION value="2012">2012</OPTION>
-	<OPTION value="2011">2011</OPTION>
-	<OPTION value="2010">2010</OPTION>
-	<OPTION value="2009">2009</OPTION>
-	<OPTION value="2008">2008</OPTION>
-    </SELECT>
-  </p>
-  </div>
-  <p class="p-container">
-    <input type="submit" value="RECHERCHER" onclick="toggle_visibility('recherche');">
-  </p>
+    <h1>WikiContrib</h1> <a href="http://fr.wikipedia.org"> <img class="wikilogo" src="img/wiki.png" /> </a>
+    <div class="inset">
+    <p>
+        <label for="nom">NOM DU COMPTE :</label>
+        <input type="text" name="nom" id="nom">
+    </p>
+    <p>
+        <label for="wikisite">QUEL SITE UTILISER :</label>
+        <SELECT name="wikisite">
+            <OPTION selected value="http://fr.wikipedia.org">fr.wikipedia.org</OPTION>
+            <OPTION value="http://en.wikipedia.org">en.wikipedia.org</OPTION>
+        </SELECT>
+    </p>
+    <p>
+        <label for="annee">POUR QUELLE ANNÉE?</label>
+        <SELECT name="annee">
+	    <OPTION value="2014">2014</OPTION>
+            <OPTION selected value="2013">2013</OPTION>
+            <OPTION value="2012">2012</OPTION>
+    	    <OPTION value="2011">2011</OPTION>
+	    <OPTION value="2010">2010</OPTION>
+	    <OPTION value="2009">2009</OPTION>
+	    <OPTION value="2008">2008</OPTION>
+        </SELECT>
+    </p>
+    </div>
+    <p class="p-container">
+        <input type="submit" value="RECHERCHER" onclick="toggle_visibility('recherche');">
+    </p>
 </form>
 
 <div id="recherche" style="display:none;" >
-<table class="recherche" border="0" bgcolor="ff0000" height="40" width="300" >
-<tr align="center">
-<td align="center"><label>ANALYSE EN COURS....S'IL VOUS PLAIT PATIENTER</label></td>
-</tr>
-</table>
+    <table class="recherche" border="0" bgcolor="ff0000" height="40" width="300" >
+        <tr align="center">
+            <td align="center"><label>ANALYSE EN COURS... VEUILLEZ PATIENTER S.V.P</label></td>
+        </tr>
+    </table>
 </div>
-
 
 <!///////////////////////////////////////////////////////////////////////>
 <script type="text/javascript">
     function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
+        var e = document.getElementById(id);
+        if(e.style.display == 'block')
+            e.style.display = 'none';
+        else
+            e.style.display = 'block';
     }
 </script>
 <!///////////////////////////////////////////////////////////////////////>
